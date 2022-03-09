2ca96dcb87d56ec63ff6c389e905f3d47ba99297
Dowha Kim <mail@dowha.kim>
2022-03-09 20:39:56 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,11 +15,12 @@ const IndexPage = () => {
       >
         Gatsby Ant Design Docs Boilerplate
       </p>
-      <h2>A gatsby starter to create documentation websites</h2>
+      <h2>Hi👋 I'm Dowha, a contents marketer who believes positive effects of internet technology. Have great passion in SaaS and Gender equality.
+I love to be creative with Adobe Illustrator, HTML/CSS, Javascript, Gatsby.</h2>
       <p>
-        This is a fork of{' '}
-        <a href="https://github.com/cvluca/gatsby-starter-markdown">
-          https://github.com/cvluca/gatsby-starter-markdown
+        You can find me on LinkedIn{' '}
+        <a href="https://www.linkedin.com/in/kimdowha/">
+          https://www.linkedin.com/in/kimdowha/
         </a>
       </p>
       <br />
@@ -27,7 +28,7 @@ const IndexPage = () => {
         <Button
           size="large"
           icon={<TwitterOutlined />}
-          href="https://twitter.com/jannikbuschke"
+          href="https://twitter.com/dowha_kim"
           target="_blank"
         >
           Twitter
@@ -35,14 +36,14 @@ const IndexPage = () => {
         <Button
           size="large"
           icon={<GithubOutlined />}
-          href="https://github.com/jannikbuschke/gatsby-antd-docs"
+          href="https://github.com/dowha"
           target="_blank"
         >
           Github
         </Button>
 
         <Button type="primary" size="large">
-          <Link to="/docs/template/get-started/introduction">Get Started</Link>
+          <Link to="https://blog.dowha.kim">Go to my Blog</Link>
         </Button>
       </Button.Group>
     </div>
